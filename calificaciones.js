332f76d5e57afe1155fda447f3fb720cb82883e1
Luz A. López <andrefire37@gmail.com>
2023-05-09 10:37:47 -0500

    v_0.0.01

---
+++ calificaciones.js
@@ -0,0 +1,7 @@
+function  calcular_definitiva(p1,p2,p3,ex,tr){
+    // to do -algoritmo no se valido antes de ejecución
+    let promedio = 0.55*(p1+p2+p3) + 0.3*ex + 0.15*tr;
+     
+    return promedio;
+
+}
